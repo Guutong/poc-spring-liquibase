62a33ff5b7d66b9c3e7ee1e37c48ac8a1eb349a1
Pornmongkon Pongsai <pornmongkonp@riverpark.co.th>
2016-08-21 02:13:11 +0700

    Add Test config

--- src/test/java/xyz/guutong/DemoApplicationTests.java
+++ src/test/java/xyz/guutong/DemoApplicationTests.java
@@ -1,16 +1,47 @@
 package xyz.guutong;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.core.NestedCheckedException;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.net.ConnectException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DemoApplicationTests {
 
+	@Rule
+	public OutputCapture outputCapture = new OutputCapture();
+
 	@Test
-	public void contextLoads() {
+	public void testDefaultSettings() throws Exception {
+		try {
+			DemoApplication.main(new String[] { "--server.port=0" });
+		}
+		catch (IllegalStateException ex) {
+			if (serverNotRunning(ex)) {
+				return;
+			}
+		}
+		String output = this.outputCapture.toString();
+		assertThat(output).contains("Successfully acquired change log lock");
+	}
+
+	@SuppressWarnings("serial")
+	private boolean serverNotRunning(IllegalStateException ex) {
+		NestedCheckedException nested = new NestedCheckedException("failed", ex) {
+		};
+		if (nested.contains(ConnectException.class)) {
+			Throwable root = nested.getRootCause();
+			if (root.getMessage().contains("Connection refused")) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
