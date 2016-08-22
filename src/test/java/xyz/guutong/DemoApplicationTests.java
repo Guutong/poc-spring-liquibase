79fa156ea01e2a69c29d6f46fa32ff73da17c7e8
Pornmongkon Pongsai <pornmongkonp@riverpark.co.th>
2016-08-22 09:30:44 +0700

    Change yaml to xml format

--- src/test/java/xyz/guutong/DemoApplicationTests.java
+++ src/test/java/xyz/guutong/DemoApplicationTests.java
@@ -14,34 +14,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DemoApplicationTests {
 
-	@Rule
-	public OutputCapture outputCapture = new OutputCapture();
-
-	@Test
-	public void testDefaultSettings() throws Exception {
-		try {
-			DemoApplication.main(new String[] { "--server.port=0" });
-		}
-		catch (IllegalStateException ex) {
-			if (serverNotRunning(ex)) {
-				return;
-			}
-		}
-		String output = this.outputCapture.toString();
-		assertThat(output).contains("Successfully acquired change log lock");
-	}
-
-	@SuppressWarnings("serial")
-	private boolean serverNotRunning(IllegalStateException ex) {
-		NestedCheckedException nested = new NestedCheckedException("failed", ex) {
-		};
-		if (nested.contains(ConnectException.class)) {
-			Throwable root = nested.getRootCause();
-			if (root.getMessage().contains("Connection refused")) {
-				return true;
-			}
-		}
-		return false;
-	}
 
 }
